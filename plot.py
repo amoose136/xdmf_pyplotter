cf12df7932ab353e8f705ca2e7076b1060fadfb9
Amos Manneschmidt <amoose136@gmail.com>
2017-01-20 17:20:01 -0500

    🔧  all the changes;🐛  fix help behavior;🐛  fix lin/log setting

--- plot.py
+++ plot.py
@@ -14,16 +14,23 @@ if six.PY2:
 start_time = time_lib.time()
 # construct main parser:
 parser = argparse.ArgumentParser(description="Plot variables from XDMF with matplotlib")
+def print_help():
+	if '-h' in sys.argv or '--help' in sys.argv:
+		parser.print_help()
+		print('\nBut also there are many settings contained in the plaintext settings file, plot.config:\n')
+		settings_parser.print_help()
+		sys.exit()
 group=parser.add_mutually_exclusive_group(required=True)
 parser.add_argument('--quiet','-q',dest='quiet',action='store_const',const=True, help='only display error messages (default full debug messages)')
 group.add_argument('--settings','-s',dest='settingsfile',help='A settings file full of plotting options')
 group.add_argument('--tree',help='Display layout of available data as found by xdmf',action='store_true',default=False)
 parser.add_argument('--threads','-t',dest='threads', help='number of threads to use for parallel operations')
 parser.add_argument('files',metavar='frame_###.xmf',nargs='+',help='xdmf files to plot using the settings files')
-
+# parser.add_argument('--files','-f',smetavar='frame_###.xmf',nargs='+',help='xdmf files to plot using the settings files')
 # create subparser for all the plot settings:
-settings_parser=argparse.ArgumentParser(description="Take input settings for matplotlib",prog='./plot.py file[01..N].xmf -s plot.config where plot.config contains:')
-# Create list of arguments that appear in the argparser so it knows to add a '-' in front if there isn't one:
+settings_parser=argparse.ArgumentParser(description="Input plot settings for matplotlib to use",prog='plot.config parser')
+# parser.add_argument('files',metavar='frame_###.xmf',nargs='+',help='xdmf files to plot using the settings files')
+# Create list of arguments that appear in the argparser so it knows to add a '-' in front if there isn't sone:
 argslist=[\
 'cmap',\
 'cbar_scale',\
@@ -64,7 +71,7 @@ try:
 	import matplotlib.pyplot as plt
 	from matplotlib.colorbar import make_axes
 	from mpl_toolkits.axes_grid1 import make_axes_locatable
-	from matplotlib.colors import LinearSegmentedColormap,is_color_like
+	from matplotlib.colors import LinearSegmentedColormap,is_color_like,LogNorm
 except ImportError:
 	eprint('Fatal Error: matplotlib or parts of it not found!')
 	sys.exit()
@@ -99,7 +106,7 @@ def check_color(value):
 	else:
 		raise argparse.ArgumentTypeError("%s is an invalid color value" % value)
 # create subparser arguments:
-settings_parser.add_argument('-variable',required=True,type=str,metavar='AttributeName',help='The attribute to plot like \'Entropy\', or \'Density\', etc. The name must match the XDMF attribute tags')
+settings_parser.add_argument('-variable',type=str,metavar='AttributeName',help='The attribute to plot like \'Entropy\', or \'Density\', etc. The name must match the XDMF attribute tags')
 settings_parser.add_argument('-cmap',default='hot_desaturated',help='Colormap to use for colorbar')#done
 settings_parser.add_argument('-background_color',type=check_color,default='white',help='color to use as background')#done
 settings_parser.add_argument('-text_color',type=check_color,default='black',help='color to use for text and annotations')
@@ -124,6 +131,7 @@ settings_parser.add_argument('-time_format',type=str,metavar='{{seconds}, s, ms,
 settings_parser.add_argument('-bounce_time_enabled',type=check_bool,choices=[True,False],metavar='{{True},False}',default=True,help='Boolean option for "time since bounce" display')
 settings_parser.add_argument('-elapsed_time_enabled',type=check_bool,choices=[True,False],metavar='{{True},False}',default=True,help='Boolean option for "elapsed time" display')
 settings_parser.add_argument('-zoom_value',type=check_float,help='The zoom value (percentage of total range) to use if the x or y range is set to \'auto\'')
+print_help()
 args=parser.parse_args()
 # define an error printing function for error reporting to terminal STD error IO stream
 def eprint(*arg, **kwargs):
@@ -133,12 +141,16 @@ def eprint(*arg, **kwargs):
 def qprint(*arg,**kwargs):
 	if not args.quiet:
 		print(*arg,**kwargs)
+#display help for just the config parser if "help" or "h" appears after -s or --settings
+if args.settingsfile and args.settingsfile in ['help','h']:
+	settings_parser.print_help()
+	sys.exit()
 # Define settings
 if args.settingsfile and args.settingsfile!='':
 	settingsargs=[]
 	for super_arg in csv.reader(open(args.settingsfile).read().split('\n'),delimiter=' ',quotechar='"',escapechar='\\'):
 		if super_arg[0][0]+super_arg[0][1]=='//':
-			break
+			continue
 		for arg in super_arg:
 			# account for the required '-' needed for argparse
 			if arg in argslist:
@@ -146,14 +158,6 @@ if args.settingsfile and args.settingsfile!='':
 			else:
 				settingsargs.append(arg)
 	settings=settings_parser.parse_args(settingsargs)
-# Carefully import xdmf
-try:
-	from Xdmf import *
-except:
-	eprint("Fatal Error: Xdmf python bindings not found")	
-	eprint("\tIf on Mac, try \"$brew install amoose136/xdmf/xdmf --HEAD\"")
-	eprint("\tThis should work for python2.x bindings but I have no guarantee for python3.x yet")
-	sys.exit()
 #Robustly import an xml writer/parser
 try:
 	from lxml import etree as et
@@ -216,8 +220,6 @@ def tree():
 	qprint()
 
 for file in args.files:
-	reader = XdmfReader.New()
-	dom = XdmfReader.read(reader,file)
 	domain=et.parse(file).getroot()[0]
 	if args.tree:
 		tree()
@@ -238,16 +240,15 @@ for file in args.files:
 		TrueGridname=gridname
 	del match
 	image_name = re.search('(?!.+\/.+)(?!\/).+',TrueVarname).group().title()+'_'+re.search('(?!.*\/).*',file).group()[:-4]+'.'+settings.image_format
-	grid = dom.getRectilinearGrid(gridname)
 	coordinates=[]
-	def get_coordinates():
-		expected_dim=grid2.find('Topology').get('NumberOfElements').split()
+	def get_coordinates( ):
+		expected_dim=grid.find('Topology').get('NumberOfElements').split()
 		try:
-			assert len(expected_dim)==int(grid2.find('Topology').get('TopologyType')[0])
+			assert len(expected_dim)==int(grid.find('Topology').get('TopologyType')[0])
 		except:
-			eprint('Error: Dimensions specified in topology tag ('+int(grid2.find('Topology').get('TopologyType')[0])+') do not match typology type ('+grid2.find('Topology').get('TopologyType')+')')
+			eprint('Error: Dimensions specified in topology tag ('+int(grid.find('Topology').get('TopologyType')[0])+') do not match typology type ('+grid.find('Topology').get('TopologyType')+')')
 			sys.exit()
-		for i,coord in enumerate(grid2.find('Geometry').findall('DataItem')):
+		for i,coord in enumerate(grid.find('Geometry').findall('DataItem')):
 			if coord.get('Dimensions'):
 				try:
 					assert expected_dim[len(expected_dim)-1-i] == coord.get('Dimensions')
@@ -287,7 +288,7 @@ for file in args.files:
 				coordinates.append(hf[coordpath.split(':')[1]][ssc['start']:end:ssc['stride']])
 
 	try:
-		grid2 = domain.find('*[@Name="'+gridname+'"]')
+		grid = domain.find('*[@Name="'+gridname+'"]')
 		get_coordinates()
 		zeniths=coordinates[0]
 		azimuths=coordinates[1]
@@ -299,15 +300,13 @@ for file in args.files:
 	rad, phi = np.meshgrid(zeniths, azimuths)
 	x,y=pol2cart(rad,phi)
 	try:
-		variable=grid.getAttribute(varname)
-		variable.read()
+		attrib_elements=domain.find("*[@Name='"+gridname+"']/*[@Name='"+varname+"']/").getchildren()
+		datapath=attrib_elements[1].text.split(':')[1]
 	except AttributeError:
 		eprint("Error: Invalid attribute")
 		eprint("\t"+settings.variable+" not found in "+file)
-		eprint("\tPath looked for was :"+gridname+"/"+varname)
+		eprint("\tPath looked for was: "+gridname+"/"+varname)
 		sys.exit()
-	attrib_elements=domain.find("*[@Name='"+gridname+"']/*[@Name='"+varname+"']/").getchildren()
-	datapath=attrib_elements[1].text.split(':')[1]
 	if attrib_elements[0].get('Dimensions'):
 		ssc=[]
 		arrsize=int(attrib_elements[0].get('Dimensions').split()[1])
@@ -403,10 +402,12 @@ for file in args.files:
 	}
 
 	# Create colorbar ("hot desaturated" in VisIt)
-
-
 	hot_desaturated=LinearSegmentedColormap('hot_desaturated',cdict,N=256,gamma=1.0)
-	pcolor=sp.pcolormesh(x, y, variable,cmap=[hot_desaturated,settings.cmap][settings.cmap!='hot_desaturated'])
+	if settings.cbar_scale=='log':
+		norm=LogNorm()
+	else:
+		norm=None
+	pcolor=sp.pcolormesh(x, y, variable,cmap=[hot_desaturated,settings.cmap][settings.cmap!='hot_desaturated'],norm=norm)
 	
 	for atr in ['title','x_range_label','y_range_label']:
 		settings.__setattr__(atr,re.sub(r'\\var(?=[^i]|$)',TrueVarname,settings.__getattribute__(atr)))
@@ -417,16 +418,20 @@ for file in args.files:
 	title=sp.set_title(settings.title,fontsize=settings.title_font_size)
 	xlabel=sp.set_xlabel(settings.x_range_label,fontsize=settings.label_font_size)
 	ylabel=sp.set_ylabel(settings.y_range_label,fontsize=settings.label_font_size)
-	
-	cbar_orientation=['vertical','horizontal'][settings.cbar_location in ['top','bottom']]
-	divider=make_axes_locatable(ax)
-	cax=divider.append_axes(settings.cbar_location,size=str([settings.cbar_width,'5'][str(settings.cbar_width)=='auto'])+"%",pad=[[.8,.4][settings.cbar_location=='top'],[.1,.8][settings.cbar_location=='left']][(cbar_orientation=='vertical')])# note that this last setting, pad, is done in a sneaky way. True + True = 2 in python. ¯\_(ツ)_/¯
-	cbar=plt.colorbar(pcolor,cax=cax,orientation=cbar_orientation)
-	# settings for colorbar ticks positioning:
-	if settings.cbar_location in ['top','bottom']:
-		cax.xaxis.set_ticks_position(settings.cbar_location)
-	else:
-		cax.yaxis.set_ticks_position(settings.cbar_location)
+	if settings.cbar_enabled==True:
+		cbar_orientation=['vertical','horizontal'][settings.cbar_location in ['top','bottom']]
+		divider=make_axes_locatable(ax)
+		
+		cax=divider.append_axes(settings.cbar_location,\
+							size=str([settings.cbar_width,'5'][str(settings.cbar_width)=='auto'])+"%",\
+							pad=[[.8,.4][settings.cbar_location=='top'],[.1,.8][settings.cbar_location=='left']][(cbar_orientation=='vertical')])# note that this last setting, pad, is done in a sneaky way. True + True = 2 in python. ¯\_(ツ)_/¯
+
+		cbar=plt.colorbar(pcolor,cax=cax,orientation=cbar_orientation)
+		# settings for colorbar ticks positioning:
+		if settings.cbar_location in ['top','bottom']:
+			cax.xaxis.set_ticks_position(settings.cbar_location)
+		else:
+			cax.yaxis.set_ticks_position(settings.cbar_location)
 
 	# fig.suptitle('this is the figure title', fontsize=12,)
 	plt.tight_layout()
